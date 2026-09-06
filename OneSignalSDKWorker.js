8568dfb32db8feb58b8c42244df3cfc50c5466ed
All-Basic-To-Advance-Tool <pdf72324@gmail.com>
2026-09-06 08:33:08 +0530

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,2 @@
 // OneSignal push notifications ke liye required file — root me hi honi chahiye
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js");
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
